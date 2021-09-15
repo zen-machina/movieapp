3d11084ff7c4bd151976830e667f7d262c17d988
zen-machina <rojosudo@gmail.com>
2021-09-14 21:38:04 -0700

    adding search functionality

--- src/Search.js
+++ src/Search.js
@@ -5,13 +5,32 @@ import React, { useState } from "react";
 // handleSubmit (onSubmit) here somewhere
 // pass input in submit state for search
 // use submit state as search query for api.js
-
 function Search() {
+    // input value of search field
+    const [inputValue, setInputValue] = useState("");
+    const [submitValue, setSubmitValue] = useState("");
+
+    // listens to target value in input field and adds to state
+    const handleChange = (evt) => {
+        setInputValue(evt.target.value);
+    };
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        setSubmitValue(inputValue);
+        console.log(submitValue);
+    };
+
     return (
         <div>
             <div>
-                <form action="">
-                    <input type="text" placeholder="Movie Name" />
+                <form action="" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        value={inputValue}
+                        placeholder="Movie Name"
+                        onChange={handleChange}
+                    />
                     <button>search</button>
                 </form>
             </div>
